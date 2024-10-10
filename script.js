e2a658e858e9098739480caab97b497c6872f06e
Jaz <jasminemaslen7@gmail.com>
2024-10-10 10:24:32 +0100

    Added basic JS functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function displayRecipe(event) {
+	event.preventDefault();
 
+	new Typewriter("#output", {
+		strings: "Recipe Recipe Recipe",
+		autoStart: true,
+		loop: false,
+		delay: 30,
+		cursor: "",
+	});
+}
+
+let button = document.querySelector("#submit");
+button.addEventListener("submit", displayRecipe);
